e6a179705fa46efb26d167d57aac814aa71992a4
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-01 13:59:58 -0500

    substantial progress; almost ready to start the actual build files

--- src/lib/app-init.mjs
+++ src/lib/app-init.mjs
@@ -2,6 +2,7 @@ import { existsSync, readFileSync } from 'node:fs'
 import * as fsPath from 'node:path'
 
 import { CATALYST_API_SPEC, CATALYST_SERVER_PLUGINS, CATALYST_HOME } from '@liquid-labs/catalyst-defaults'
+import { LIQ_PLAYGROUND } from '@liquid-labs/liq-defaults'
 import { appInit as superInit } from '@liquid-labs/plugable-express'
 
 const packageJSONPathProd = fsPath.resolve(__dirname, '..', 'package.json')
@@ -16,6 +17,7 @@ const appInit = async({
   name = 'catalyst-server',
   cliName = 'catalyst',
   version = pkgVersion,
+
   apiSpecPath = CATALYST_API_SPEC(),
   pluginsPath = CATALYST_SERVER_PLUGINS(),
   pluginPaths = [myPackagePath],
@@ -25,21 +27,23 @@ const appInit = async({
       url  : 'raw.githubusercontent.com/liquid-labs/catalyst-registry/main/registry.yaml'
     }
   ],
+  devPaths = [LIQ_PLAYGROUND()],
   serverHome = CATALYST_HOME(),
   useDefaultSettings = true,
   ...options
 }) => {
   const results =
     await superInit({
-      name,
-      cliName,
-      version,
       apiSpecPath,
+      cliName,
+      devPaths,
+      name,
       pluginsPath,
       pluginPaths,
       defaultRegistries,
       serverHome,
       useDefaultSettings,
+      version,
       ...options
     })
 
