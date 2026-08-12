7c8510f03368ded666bdfa47da038b69b99e762f
Zane Rockenbaugh <zane@liquid-labs.com>
2026-08-11 20:12:39 -0500

    fix: migrate to registerPathVar API for path variable registration
    plugable-express removed app.ext.pathResolvers in favor of an
    explicit registerPathVar(name, { validationRe, optionsFetcher })
    function injected into setup(). Update the workKey registration in
    src/setup.mjs accordingly (bitReString -> validationRe field rename);
    plugin was crashing on startup with the old dead API.

--- src/setup.mjs
+++ src/setup.mjs
@@ -2,16 +2,16 @@ import * as fsPath from 'node:path'
 
 import { WorkDB } from './handlers/work/_lib/work-db'
 
-const setup = ({ app, reporter }) => {
+const setup = ({ app, reporter, registerPathVar }) => {
   app.ext.constants.WORK_DB_PATH = fsPath.join(app.ext.serverConfigRoot, 'work', 'work-db.yaml')
 
-  app.ext.pathResolvers.workKey = {
+  registerPathVar('workKey', {
     optionsFetcher : () => {
       const workDB = new WorkDB({ app })
       return workDB.getWorkKeys()
     },
-    bitReString : 'work-[^/]+(?:/|%2[Ff])[^/]+(?:/|%2[Ff])[0-9]+'
-  }
+    validationRe : 'work-[^/]+(?:/|%2[Ff])[^/]+(?:/|%2[Ff])[0-9]+'
+  })
 }
 
 export { setup }
